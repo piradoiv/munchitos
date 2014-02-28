9c399135c9df59ddaec3357edbbf684efe16209d
Ricardo Cruz <piradoiv@gmail.com>
2014-02-28 13:02:13 +0000

    Add testCanFetchLinks

---
+++ tests/PiradoIV/Munchitos/Test/LinksTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace PiradoIV\Munchitos\Test;
+
+use \PiradoIV\Munchitos\Munchitos;
+
+class LinksTest extends TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        $this->html = <<<HTML
+<!doctype html>
+<html>
+  <body>
+    <p>This is an example of <a href="http://www.example.org/" target="_blank">a link</a></p>
+    <p>This is <a href="internal-page.html">another link</a>.
+  </body>
+</html>
+HTML;
+
+        $this->munchitos = new Munchitos;
+    }
+
+    public function testCanFetchLinks()
+    {
+        $links = $this->munchitos->links();
+        $this->assertEquals(2, count($links));
+    }
+}
