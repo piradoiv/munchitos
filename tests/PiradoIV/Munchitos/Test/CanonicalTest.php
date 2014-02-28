b8b68c398727c3010618f5aaf8f1fd459d21d884
Ricardo Cruz <piradoiv@gmail.com>
2014-02-28 10:58:27 +0000

    Add testCanFetchCanonicalUrl

---
+++ tests/PiradoIV/Munchitos/Test/CanonicalTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace PiradoIV\Munchitos\Test;
+
+use \PiradoIV\Munchitos\Munchitos;
+
+class CanonicalTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->canonical = 'http://www.example.org/';
+        $this->html = <<<HTML
+<!doctype html>
+<html>
+  <head>
+    <link rel="canonical" href="{$this->canonical}" />
+  </head>
+</html>
+HTML;
+
+        $this->munchitos = new Munchitos;
+        $this->munchitos->html($this->html);
+    }
+
+    public function testCanFetchCanonicalUrl()
+    {
+        $this->assertEquals($this->canonical, $this->munchitos->canonical());
+    }
+}
