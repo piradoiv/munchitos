e4a9d2053e746296f5ac84e9beb7e0b61bae7201
Ricardo Cruz <piradoiv@gmail.com>
2014-02-28 09:21:48 +0000

    Pass testCanAddAndGetHtml

--- src/PiradoIV/Munchitos/Munchitos.php
+++ src/PiradoIV/Munchitos/Munchitos.php
@@ -4,10 +4,22 @@ namespace PiradoIV\Munchitos;
 
 class Munchitos
 {
+    protected $html;
+
     /**
      * Create a new Munchitos Instance
      */
     public function __construct()
     {
+    }
+
+    public function setHtml($html = null)
+    {
+        $this->html = $html;
+    }
+
+    public function getHtml()
+    {
+        return $this->html;
     }
 }
