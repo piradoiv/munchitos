098e5e8fb45b06b1d87d4eec7e89d0362df60194
Ricardo Cruz <piradoiv@gmail.com>
2014-02-28 11:20:26 +0000

    Add testCanFetchListOfStyleSheets

---
+++ tests/PiradoIV/Munchitos/Test/CssTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace PiradoIV\Munchitos\Test;
+
+use \PiradoIV\Munchitos\Munchitos;
+
+class CssTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->html = <<<HTML
+<!doctype html>
+<html>
+  <head>
+    <link href='/fonts/entypo.css' rel='stylesheet' type='text/css' />
+    <link href='css/app.css' rel="stylesheet" type="text/css" />
+  </head>
+</html>
+HTML;
+
+        $this->munchitos = new Munchitos;
+        $this->munchitos->html($this->html);
+    }
+
+    public function testCanFetchListOfStyleSheets()
+    {
+        $stylesheets = $this->munchitos->stylesheets();
+        $this->assertEquals(2, count($stylesheets));
+    }
+}
